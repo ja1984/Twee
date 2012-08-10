8983b5e6b586dee9149d29f3abb9ae9763ec80e7
Jonathan Andersson <ja1984@gmail.com>
2012-08-11 01:52:57 +0200

    Got the gitignore to work

---
+++ src/se/ja1984/teewee/DateService.java
@@ -0,0 +1,99 @@
+package se.ja1984.teewee;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+
+public class DateService {
+ 
+//	public Date getDateNearest(ArrayList<Episode> episodes){
+//		GregorianCalendar d = new GregorianCalendar(2012, 7, 21);
+//        return new TreeSet<Episode>(episodes).lower(d);
+//}
+	
+	
+	public String Episodenumber(Episode episode)
+	{
+		String s = episode.getSeason();
+		String e = episode.getEpisode();
+		
+		e = e.length() < 2 ? "0" + e : e;
+		s = s.length() < 2 ? "0" + s : s;
+		
+		return "S" + s + "E" + e;
+	}
+	
+	public int CompareDates(String d1, String d2)
+	{
+		SimpleDateFormat df = new SimpleDateFormat ("yyyy-MM-dd");
+		DateFormat format = DateFormat.getDateInstance();
+		
+		Date date1 = null;
+		Date date2 = null;
+		
+		try {
+			date1 = df.parse(d1);
+			date2 = df.parse(d2);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return date1.compareTo(date2);
+	}
+	
+	public String DisplayDate(String date)
+	{
+		String returndate = null;
+		Date today = setTimeToMidnight(new Date());
+		Date d1 = null;
+		SimpleDateFormat df = new SimpleDateFormat ("yyyy-MM-dd");
+		DateFormat format = DateFormat.getDateInstance();
+	
+		try {
+			d1 = df.parse(date);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		if(d1 == null)
+			return date;
+		
+		switch (d1.compareTo(today)) {
+		case -1:
+			returndate = "Aired " + format.format(d1);
+			break;
+		case 0:
+			returndate = "Airs today";
+			break;
+		default:
+			returndate = "Airs " + format.format(d1);
+			break;
+		}
+	
+		
+		
+		return returndate;
+	}
+	
+	private static Date setTimeToMidnight(Date date) {
+	    Calendar calendar = Calendar.getInstance();
+
+	    calendar.setTime( date );
+	    calendar.set(Calendar.HOUR_OF_DAY, 0);
+	    calendar.set(Calendar.MINUTE, 0);
+	    calendar.set(Calendar.SECOND, 0);
+	    calendar.set(Calendar.MILLISECOND, 0);
+
+	    return calendar.getTime();
+	}
+	
+	public String GetTodaysDate()
+	{
+		return android.text.format.DateFormat.format("yyyy-MM-dd", new java.util.Date()).toString();
+	}
+	
+}
---
+++ src/se/ja1984/teewee/SettingsActivity.java
@@ -0,0 +1,115 @@
+package se.ja1984.teewee;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.res.Resources.Theme;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class SettingsActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        SharedPreferences settings = getSharedPreferences("Twee", 0);
+	    int theme = settings.getInt("Theme", R.style.Light);
+	    
+	    RadioButton oldCheckedButton = null;
+	    
+        if((int) theme == R.style.Light)
+        {
+        	oldCheckedButton = (RadioButton)findViewById(R.id.radioTheme0);
+        }
+        else if(theme == R.style.LightDark)
+        {
+        	oldCheckedButton = (RadioButton)findViewById(R.id.radioTheme1);
+        }       
+        else
+        {
+        	oldCheckedButton = (RadioButton)findViewById(R.id.radioTheme2);
+        }
+        	
+        oldCheckedButton.setChecked(true);
+        
+        
+        
+     // This will get the radiogroup
+        RadioGroup radioGroup = (RadioGroup)findViewById(R.id.grpTheme);
+        // This will get the radiobutton in the radiogroup that is checked
+        RadioButton checkedRadioButton = (RadioButton)radioGroup.findViewById(radioGroup.getCheckedRadioButtonId());
+        
+        
+     // This overrides the radiogroup onCheckListener
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup rGroup, int checkedId)
+            {
+                // This will get the radiobutton that has changed in its check state
+                RadioButton checkedRadioButton = (RadioButton)rGroup.findViewById(checkedId);
+                // This puts the value (true/false) into the variable
+                boolean isChecked = checkedRadioButton.isChecked();
+                // If the radiobutton that has changed in check state is now checked...
+                if (isChecked)
+                {
+                    // Changes the textview's text to "Checked: example radiobutton text"
+                	saveTheme(checkedRadioButton.getText().toString());
+                    //Log.d("Checked", checkedRadioButton.getText().toString());
+                }
+            }
+        });
+        
+        
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_settings, menu);
+        return true;
+    }
+
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void saveTheme(String selectedTheme)
+    {
+    	
+    	int option = 0;
+    	
+    	if(selectedTheme.equals("Light theme"))
+    	{
+    		option = R.style.Light;
+    	}
+    	else if(selectedTheme.equals("Light theme w. dark actionbar"))
+    	{
+    		option = R.style.LightDark;
+    	}	
+    	else
+    	{
+    		option = R.style.Dark;
+    	}
+    	
+    	SharedPreferences settings = getSharedPreferences("Twee", 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("Theme", option);
+
+        // Commit the edits!
+        editor.commit();
+    }
+    
+}
